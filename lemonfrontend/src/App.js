73bea1aab14de11bb10f83f76b4bfb669ccc5a37
maridi manoj kumar <21a91a6152@aec.edu.in>
2024-11-26 14:36:00 +0530

    protectdroutes

--- lemonfrontend/src/App.js
+++ lemonfrontend/src/App.js
@@ -1,34 +1,3 @@
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import Home from "./components/Home";
-// import SignIn from "./components/login";
-// import ProductsDashboard from "./components/Products";
-// import FarmersDashboard from "./components/Farmers";
-// import PurchaseDashboard from "./components/Purchase";
-// import { BrowserRouter,Route,Routes } from 'react-router-dom';
-
-// function App() {
- 
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Navbar   />
-//         <Routes>
-          
-//           <Route path="/" element={<Home/>}/>
-//           <Route path="/login" element={<SignIn/>}/>
-//           <Route path="/products" element={<ProductsDashboard/>}/>
-//           <Route path="/vendors/farmers" element={<FarmersDashboard/>}/>
-//           <Route path="/purchase" element={<PurchaseDashboard/>}/>
-//         </Routes>
-//         <Footer/>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;.
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -36,29 +5,33 @@ import SignIn from "./components/login";
 import ProductsDashboard from "./components/Products";
 import FarmersDashboard from "./components/Farmers";
 import PurchaseDashboard from "./components/Purchase";
-import { BrowserRouter,Route,Routes ,Navigate} from 'react-router-dom';
- 
- 
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
 const App = () => {
   const isAuthenticated = localStorage.getItem("userData");
 
   return (
-    <div> 
-      <BrowserRouter> 
-      <Navbar></Navbar>
-    <Routes>
-       
-      <Route
-        path="/"
-        element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
-      />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/products" element={<ProductsDashboard />} />
-      <Route path="/vendors/farmers" element={<FarmersDashboard />} />
-      <Route path="/purchase" element={<PurchaseDashboard />} />
-    </Routes>
-    <Footer></Footer>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        {isAuthenticated ? (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<ProductsDashboard />} />
+              <Route path="/vendors/farmers" element={<FarmersDashboard />} />
+              <Route path="/purchase" element={<PurchaseDashboard />} />
+              <Route path="*" element={<Navigate to="/" />} /> {/* Redirect any non-authenticated routes */}
+            </Routes>
+            <Footer />
+          </>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<SignIn />} />
+            <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login for any route */}
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 };
